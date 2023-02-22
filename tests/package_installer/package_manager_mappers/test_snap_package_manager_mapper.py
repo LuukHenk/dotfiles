74ccd532c756aecc3e9f1af48059be11af712207
luukhenk <luukperdaems@hotmail.com>
2023-02-22 13:41:45 +0100

    added test setup

---
+++ tests/package_installer/package_manager_mappers/test_snap_package_manager_mapper.py
@@ -0,0 +1,10 @@
+
+from unittest import TestCase, main
+
+class TestSnapPackageManagerMapper(TestCase):
+
+    def test_something(self):
+        assert(True)
+
+if __name__ == '__main__':
+    main()
