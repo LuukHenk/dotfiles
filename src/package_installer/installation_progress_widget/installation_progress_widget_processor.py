bb2c1095ac8a63c9398b8f575423fe3dddb149bf
LuukHenk <luukperdaems@hotmail.com>
2023-06-07 13:56:24 +0200

    added package accessor data layer

--- src/package_installer/installation_progress_widget/installation_progress_widget_processor.py
+++
@@ -1,8 +0,0 @@
-
-from typing import Dict, List
-
-from data_models.package_info import PackageInfo
-
-class InstallationProgressWidgetProcessor:
-    def __init__(self, package_groups: Dict[str, List[PackageInfo]]):
-        self.__package_groups = package_groups
