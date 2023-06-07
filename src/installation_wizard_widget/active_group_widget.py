2d476d384fb76e5aa3ba994b22705279c2c5bbcc
LuukHenk <luukperdaems@hotmail.com>
2023-06-07 15:01:45 +0200

    wizard widget improvements

--- src/installation_wizard_widget/active_group_widget.py
+++ src/installation_wizard_widget/active_group_widget.py
@@ -10,24 +10,26 @@ from PySide6.QtWidgets import (
     QStackedWidget,
 )
 from PySide6.QtCore import Signal
+from PySide6.QtGui import QFont
 
 from data_models.manager import Manager
 from data_models.package_info import PackageInfo
 from data_models.version import Version
 from installation_wizard_widget.package_info_text_generator import generate_package_info_text
 
-class ActiveGroupWidget(QStackedWidget):
 
+class ActiveGroupWidget(QStackedWidget):
     InstallationRequestUpdate = Signal(PackageInfo)
 
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.__groups: Dict[str, QWidget] = {}
+        self.__title_point_size = QFont().pointSize() * 2
 
     def add_group(self, group_name: str, packages: List[PackageInfo]) -> None:
         group = QWidget()
         group_layout = QVBoxLayout(group)
-        group_layout.addWidget(QLabel(group_name))
+        group_layout.addWidget(self.__create_header(group_name))
         for package in packages:
             group_layout.addWidget(self.__create_package_checkbox(package))
         self.__groups[group_name] = group
@@ -41,11 +43,16 @@ class ActiveGroupWidget(QStackedWidget):
         checkbox.clicked.connect(lambda: self.__on_package_checkbox_clicked(package_info))
         return checkbox
 
+    def __create_header(self, text: str) -> QLabel:
+        header = QLabel(text)
+        header.setStyleSheet(f"font-size: {self.__title_point_size}; font-weight: bold;")
+        return header
+
     def __on_package_checkbox_clicked(self, package_checkbox_name: PackageInfo) -> None:
         self.InstallationRequestUpdate.emit(package_checkbox_name)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     packages_ = [
         PackageInfo(
             name="python3",
