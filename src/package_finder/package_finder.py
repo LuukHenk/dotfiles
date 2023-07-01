5b0a2ccf834cdb2952e2b9ce0a4171773b7119c9
LuukHenk <luukperdaems@hotmail.com>
2023-07-01 12:08:57 +0200

    decoupled the package parser from the package finder

--- src/package_finder/package_finder.py
+++ src/package_finder/package_finder.py
@@ -6,23 +6,20 @@ from data_models.package_search_request import PackageSearchRequest
 from package_finder.package_managers.package_manager_finder import PackageManagerFinder
 from package_finder.package_managers.apt_package_manager_finder import AptPackageManagerFinder
 from package_finder.package_managers.snap_package_manager_finder import SnapPackageManagerFinder
-from package_finder.package_search_request_parser import PackageSearchRequestParser
 
 
 class PackageFinder:
     def __init__(self) -> None:
         self.__package_managers: List[PackageManagerFinder] = [AptPackageManagerFinder(), SnapPackageManagerFinder()]
-        self.__package_search_requests = PackageSearchRequestParser().package_search_requests
 
-    def get_package_info(self) -> Dict[str, List[PackageInfo]]:
+    def get_package_info(self, search_requests: List[PackageSearchRequest]) -> Dict[str, List[PackageInfo]]:
         package_info = {}
-        for search_requests in self.__package_search_requests:
+        for search_request in search_requests:
             package_group = []
             for manager in self.__package_managers:
-                for package_name in search_requests.search_query:
-                    package_group += manager.find_package(package_name, search_requests.package_group)
-            package_info[search_requests.package_group] = package_group
-
+                for package_name in search_request.search_query:
+                    package_group += manager.find_package(package_name, search_request.package_group)
+            package_info[search_request.package_group] = package_group
         return package_info
 
     def _get_package_info(self, search_requests: List[PackageSearchRequest]) -> List[PackageInfo]:
@@ -30,4 +27,5 @@ class PackageFinder:
         for search_request in search_requests:
             for manager in self.__package_managers:
                 for package_identifier in search_request.search_query:
-                    packages = manager.find_package(package_identifier, search_request.package_group)
+                    packages_info += manager.find_package(package_identifier, search_request.package_group)
+        return packages_info
--- src/qt_application/factory.py
+++ src/qt_application/factory.py
@@ -1,12 +1,14 @@
 from data_layer.package_accessor import PackageAccessor
 from installation_wizard_widget.installation_wizard_widget import InstallationWizardWidget
 from package_finder.package_finder import PackageFinder
+from package_finder.package_search_request_parser import PackageSearchRequestParser
 from package_installer.installation_progress_widget.installation_progress_widget import InstallationProgressWidget
 from package_installer.packages_installer import PackagesInstaller
+
+
 class Factory:
     def __init__(self):
-        package_groups = PackageFinder().get_package_info()
-        self.__package_accessor = PackageAccessor(package_groups)
+        self.__package_accessor = self.__construct_package_accessor()
 
     def create_installation_wizard_widget(self) -> InstallationWizardWidget:
         return InstallationWizardWidget(self.__package_accessor)
@@ -14,3 +16,9 @@ class Factory:
     def create_installation_progress_widget(self) -> InstallationProgressWidget:
         packages_installer = PackagesInstaller(self.__package_accessor)
         return InstallationProgressWidget(packages_installer)
+
+    @staticmethod
+    def __construct_package_accessor() -> PackageAccessor:
+        package_search_request_parser = PackageSearchRequestParser()
+        package_groups = PackageFinder().get_package_info(package_search_request_parser.package_search_requests)
+        return PackageAccessor(package_groups)
