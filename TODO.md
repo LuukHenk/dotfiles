4b2e5098b885e9a007c64b6bf7909366e1f240d8
LuukHenk <luukperdaems@hotmail.com>
2023-07-14 11:09:47 +0200

    added logging for loading the packages

--- TODO.md
+++ TODO.md
@@ -7,14 +7,10 @@
 
 # TODO for next version (ordered on priority)
 
-
-
-- #0000014: Display the loading of packages (in terminal for now)
-- 
 - #0000011: Add the dotfiles
 
-- #0000013: Move the groups to the top of the widget
-- - #0000008: Display if the user is root when running installation wizard
+- #0000013: Styling of the widget in more detail
+- #0000008: Display if the user is root when running installation wizard
 - #0000005: Update the readme and setup.cfg
   - https://packaging.python.org/en/latest/tutorials/packaging-projects/
 - #0000003: Write tests
--- src/configuration_loader/configuration_loader.py
+++ src/configuration_loader/configuration_loader.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 from configuration_loader.parsers.package_parser import PackageParser
 from data_layer.package_accessor import PackageAccessor
-from logger.logger import log_error
+from logger.logger import log_error, log_info
 from utils.root_finder import ROOT
+
 
 class ConfigurationLoader:
     __default_config_path = ROOT / "configuration"
@@ -12,7 +13,10 @@ class ConfigurationLoader:
         self.__packages_config_path = configuration_path / "packages.toml"
 
     def load_packages(self, package_accessor: PackageAccessor) -> None:
-        for package in PackageParser().parse(self.__packages_config_path):
+        packages = PackageParser().parse(self.__packages_config_path)
+        log_info(f"Found {len(packages)} packages in '{self.__packages_config_path}'")
+        for i, package in enumerate(packages):
+            log_info(f"Loading packages: {i+1}/{len(packages)}")
             result = package_accessor.add_package(package)
             if not result.success:
                 log_error(f"Failed to load package '{package.name}': {result.message}")
