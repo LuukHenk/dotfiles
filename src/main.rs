f051770c762eee6e839ac76097b23a384e02ad67
Luuk Perdaems <LuukPerdaems@hotmail.com>
2024-04-26 15:52:53 +0200

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+   dotfiles: Dotfiles
+}
+
+#[derive(Deserialize)]
+struct Dotfiles {
+   name: String,
+   repo_path: String,
+   deploy_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config: Config = toml::from_str(r#"
+        [[dotfiles]]
+        name="Bashrc"
+        repo_path="./bashrc"
+        deploy_path="~/.bashrc"
+    "#).unwrap();
+    println!("hello world")
 }
