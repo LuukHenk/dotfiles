532f46a8eb67c80c1e18cbb9824fa8ccf8bb55a4
Luuk Perdaems <LuukPerdaems@hotmail.com>
2024-04-26 16:32:23 +0200

    created config manager

---
+++ src/config_manager.rs
@@ -0,0 +1,46 @@
+use serde::Deserialize;
+use std::process::Command;
+
+
+#[derive(Deserialize)]
+struct Config {
+   programs: Vec<String>,
+   dotfiles: Vec<Dotfile>
+}
+
+#[derive(Deserialize)]
+struct Dotfile {
+   name: String,
+   repo_path: String,
+   deploy_path: String,
+}
+
+pub struct ConfigManager {
+    config: Config
+}
+
+impl ConfigManager{
+    pub fn new(config_str: &str) -> ConfigManager {
+        let config: Config = toml::from_str(config_str).unwrap();
+        ConfigManager{config}
+    }
+
+    pub fn set_dotfiles(&self) {
+        for dotfile in self.config.dotfiles.iter() {
+            ConfigManager::copy_file(&dotfile.repo_path, &dotfile.deploy_path)
+        }
+    }
+
+    pub fn install_programs(&self) {
+        println!("Dummy; installing programs")
+
+    }
+
+    fn copy_file(source: &String, destination: &String) {
+        println!("Copying {source} to {destination}");
+        Command::new("cp")
+        .args([source, destination])
+        .spawn()
+        .expect("failed to copy file");
+    }
+}
