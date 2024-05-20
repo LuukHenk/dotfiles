223c0fac09d7843c34106943408938c332a81228
Luuk Perdaems <LuukPerdaems@hotmail.com>
2024-05-20 15:29:57 +0200

    use io to get the config

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,37 +1,14 @@
 use dotfiles::ConfigManager;
+use dotfiles::IoOperations;
 use dotfiles::UbuntuIoOperations;
 
+const CONFIG_PATH: &str = "configuration.toml";
+
 fn main() {
-    let config_str: &str = r#"
-    programs = [
-        "python3",
-        "spotify",
-        "nautilus-dropbox",
-        "steam",
-        "signal-desktop",
-        "terminator",
-        "nvim",
-        "ncdu",
-        "wget",
-        "htop",
-        "rustup"
-    ]
-    
-    [[dotfiles]]
-    name="test home dir"
-    repo_path="conf/test.txt"
-    deploy_path="~/result.txt"
-
-
-    [[dotfiles]]
-    name="test empty path"
-    repo_path="conf/test.txt"
-    deploy_path=""
-
-    "#;
-
-    let config_manager = ConfigManager::new(config_str);
     let mut io_operations = UbuntuIoOperations {};
+    let config_string = io_operations.read_file(CONFIG_PATH);
+    let config_manager = ConfigManager::new(config_string.as_str());
+
     config_manager.set_dotfiles(&mut io_operations);
     config_manager.install_programs(&mut io_operations);
 }
